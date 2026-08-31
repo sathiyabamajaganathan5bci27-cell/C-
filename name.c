96128e07da880d0b7f1b973944a5280d395b21d2
sathiyabamajaganathan5bci27-cell <sathiyabamajaganathan5_bci27@mepcoeng.ac.in>
2026-08-31 12:07:11 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Declare variables to store the three integers
+    int a, b, c;
+    
+    // Read the three space-separated integers from stdin
+    cin >> a >> b >> c;
+    
+    // Calculate the sum
+    int sum = a + b + c;
+    
+    // Print the sum to stdout followed by a newline
+    cout << sum << endl;
+    
+    return 0;
+}
